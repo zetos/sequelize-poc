4191556e4450bea276f1269c0dde834d748ddd37
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-24 19:01:59 -0300

    lecturer controller & routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const classroomController = require('../controllers').classroom;
 const studentController = require('../controllers').student;
+const lecturerController = require('../controllers').lecturer;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,5 +23,12 @@ router.get('/api/student/:id', studentController.getById);
 router.post('/api/student', studentController.add);
 router.put('/api/student/:id', studentController.update);
 router.delete('/api/student/:id', studentController.delete);
+
+// Lecturer
+router.get('/api/lecturer', lecturerController.list);
+router.get('/api/lecturer/:id', lecturerController.getById);
+router.post('/api/lecturer', lecturerController.add);
+router.put('/api/lecturer/:id', lecturerController.update);
+router.delete('/api/lecturer/:id', lecturerController.delete);
 
 module.exports = router;
