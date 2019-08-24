78a947ff64575b727be8918f8cb077c6fc7745c1
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-24 18:59:43 -0300

    student controller & routes

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 const classroomController = require('../controllers').classroom;
+const studentController = require('../controllers').student;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Classroom
 router.get('/api/classroom', classroomController.list);
 router.get('/api/classroom/:id', classroomController.getById);
 router.post('/api/classroom', classroomController.add);
 router.put('/api/classroom/:id', classroomController.update);
 router.delete('/api/classroom/:id', classroomController.delete);
+
+// Student
+router.get('/api/student', studentController.list);
+router.get('/api/student/:id', studentController.getById);
+router.post('/api/student', studentController.add);
+router.put('/api/student/:id', studentController.update);
+router.delete('/api/student/:id', studentController.delete);
 
 module.exports = router;
