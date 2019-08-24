86431d9e5158d3030489f2fb17139cc31399c7b3
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-24 20:47:08 -0300

    Student model fix

--- models/classroom.js
+++ models/classroom.js
@@ -1,13 +1,17 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const Classroom = sequelize.define('Classroom', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
     class_name: DataTypes.STRING
   }, {});
+
   Classroom.associate = function(models) {
     Classroom.hasMany(models.Student, {
       foreignKey: 'classroom_id',
       as: 'students',
     });
   };
+
   return Classroom;
 };
