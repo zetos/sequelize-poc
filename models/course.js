3a8904764486d2c835de21dc50c7639cc9ec8b34
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-24 20:55:22 -0300

    model fixes

--- models/course.js
+++ models/course.js
@@ -1,16 +1,22 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const Course = sequelize.define('Course', {
     lecturer_id: DataTypes.INTEGER,
     course_name: DataTypes.STRING
   }, {});
+
   Course.associate = function(models) {
     Course.belongsToMany(models.Student, {
       through: 'StudentCourse',
       as: 'students',
       foreignKey: 'course_id'
     });
-    Course.belongsTo(models.Lecturer);
+    Course.belongsTo(models.Lecturer, {
+      foreignKey: 'lecturer_id',
+      as: 'lecturer'
+    });
   };
+
   return Course;
 };
--- models/lecturer.js
+++ models/lecturer.js
@@ -1,13 +1,16 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const Lecturer = sequelize.define('Lecturer', {
     lecturer_name: DataTypes.STRING
   }, {});
+
   Lecturer.associate = function(models) {
     Lecturer.hasOne(models.Course, {
       foreignKey: 'lecturer_id',
-      as: 'lecturer',
+      as: 'course',
     });
   };
+
   return Lecturer;
 };
